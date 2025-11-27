85a7ff236946e168283fcca6890a83d16bec3fb0
TakeruMatsushima <sesimu3678@gmail.com>
2025-11-27 17:47:38 +0900

    Change char code of db

---
+++ frontend/types/url-import.d.ts
@@ -0,0 +1,12 @@
+declare module '*.js?url' {
+  const url: string;
+  export default url;
+}
+declare module '*.mjs?url' {
+  const url: string;
+  export default url;
+}
+declare module 'pdfjs-dist/legacy/build/pdf.worker.min.js?url' {
+  const url: string;
+  export default url;
+}
